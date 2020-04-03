018a68847c341bc9f01988e11c61b6962220a7a9
Kill Toxic <913087364@qq.com>
2020-04-03 14:39:45 +0800

    rotationAndZoom

--- rotationAndZoom/rotationAndZoom.cpp
+++ rotationAndZoom/rotationAndZoom.cpp
@@ -1,20 +1,25 @@
-﻿// rotationAndZoom.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("D:\\Files\\lena.jpg");//读取图像
+	if (srcMat.empty())//检测是否读取成功
+	{
+		return -1;
+	}
+	//旋转-40℃，缩放尺度为
+	float angle = -10.0, scale = 1;
+	//旋转中心为图像中心
+	cv::Point2f center(srcMat.cols*0.5, srcMat.rows*0.5);
+	//获得变换矩阵
+	const cv::Mat affine_matrix = cv::getRotationMatrix2D(center, angle, scale);
+	cv::warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());//对图像进行旋转处理
+	cv::imshow("src", srcMat);//显示原图像
+	cv::imshow("dst", dstMat);//显示旋转后图像
+	waitKey(0);
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
